4046f8c2fb860e64731c3c743d9c3e4898628148
ana04mart <anaclaramartinspimentel84@gmail.com>
2021-07-07 22:27:02 -0300

    Criação da classe pessoa

--- src/classes/Pessoa.java
+++ src/classes/Pessoa.java
@@ -1,5 +1,43 @@
 package classes;
 
 public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+
+    public Pessoa (){
+
+    }
     
+    public Pessoa (String nome, String sobrenome, String telefone){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+
+    } 
+    public String getNome(){
+            return nome; 
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+   public void setNome (String nome){
+    this.nome = nome;
+}
+
+   public String getSobrenome(){
+    return sobrenome;
+}
+    public void setSobrenome(String sobrenome){
+    this.sobrenome = sobrenome;
+}
+  
+
+
+
 }
